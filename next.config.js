21934b23303435de38438dad1bebc3cd02318f2a
sasegura <sasegura.fernandez87@gmail.com>
2024-10-25 13:32:26 +0200

    asas

--- next.config.js
+++ next.config.js
@@ -3,11 +3,14 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "**",
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
+  distDir: 'build',
+  output: 'export',
+  assetPrefix: './',
 };
 
 module.exports = nextConfig;
